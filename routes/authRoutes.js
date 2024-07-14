51baf65257fe48ad9b12dae65dceb7e6fb700c24
Amro Soliman <247kod@gmail.com>
2024-07-14 01:20:23 +0100

    Adds the User Model as well the Auth Routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,23 @@
+// routes/authRoutes.js
+const express = require('express');
+const { registerUser, loginUser, logoutUser, getUserProfile } = require('../controllers/authController');
+const { protect } = require('../middlewares/authMiddleware');
+const methodCheck = require('../middlewares/methodCheckMiddleware');
+
+const router = express.Router();
+
+// Route for user registration
+router.post('/register', methodCheck('POST'), registerUser);
+
+// Route for user login
+router.post('/login', methodCheck('POST'), loginUser);
+
+// Route for user profile
+router.get('/me', methodCheck('GET'), protect, getUserProfile);
+
+// Route for user logout
+router.post('/logout', methodCheck('POST'), protect, logoutUser);
+
+
+
+module.exports = router;
